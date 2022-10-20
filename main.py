6ff9817aed63ae8dd67c827f9905d8efd246f931
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-20 17:11:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+prijs1 = float(input('wat is de prijs '))
+korting1 = int(input('wat is de korting '))
+prijs2 = prijs1 / 100
+korting2 = prijs2 * korting1
+prijs3 = prijs1  - korting2
+print ('je betaaalt', prijs3, "euro" )
