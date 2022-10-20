8377bfc8d6c5a0dde56da730b3b3d0c5526d79db
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-20 17:15:17 +0000

    round output

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ korting1 = int(input('wat is de korting '))
 prijs2 = prijs1 / 100
 korting2 = prijs2 * korting1
 prijs3 = prijs1  - korting2
-print ('je betaaalt', prijs3, "euro" )
+print ('je betaaalt', round(prijs3, 2), "euro" )
